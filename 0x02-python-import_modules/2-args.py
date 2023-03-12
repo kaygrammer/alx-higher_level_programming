05fa02bf6013fb276f861f6053d46bc1239715f3
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-03-12 20:19:46 +0100

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """print the number of and list of arguments passed to the terminal"""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
