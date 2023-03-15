3b6687df4b4670ce56ff1a8188b58804c57afecf
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-03-15 10:47:09 +0100

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,8 @@
 def print_matrix_integer(matrix=[[]]):
     for ele in matrix:
         for num in ele:
-            print("{}".format(num), end=" ")
+            if num == ele[-1]:
+                print("{}".format(num), end=" ")
+            else:
+                print("{}".format(num), end=" ")
         print()
