ab712cb4e50462c75c4aed094785c7f3ea84adbc
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-03-17 17:19:43 +0100

    uniq number

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list (only once for each integer)."""
+    sum = 0
+    uniques = set(my_list)
+    for i in uniques:
+        sum += i
+    return sum
