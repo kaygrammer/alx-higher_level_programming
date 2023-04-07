a7a71d18527a085aeae8d995777f76b7142ea105
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-07 15:16:48 +0100

    safe print integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    interger = 0
+    for i in range(x):
+        try:
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                interger += 1
+        except(ValueError, TypeError):
+            continue
+    print("")
+    return (interger)
