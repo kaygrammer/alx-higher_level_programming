c77c770ba1c7b10433c8c96f9ee4cdf84761aff8
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-07 15:27:23 +0100

    safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print("inside result: {}".format(div))
+        return div
