0b1829c3fa98521c8d4bb846081b0014c7a5f14a
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-11 14:44:43 +0100

    task 100

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def safe_function(fct, *args):
+    """
+    execute a function safely
+    """
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return (None)
