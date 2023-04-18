f67fecc0de06e3b1ba3237133b04643db0646e51
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-18 03:23:41 +0100

    init size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""an empty class that represent a square class"""
+
+
+class Square:
+    """this inititalize size and do nothing"""
+    def __init__(self, size):
+        self.__size = size
