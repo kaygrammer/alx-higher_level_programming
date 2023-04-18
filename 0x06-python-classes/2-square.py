8aaff7e96ff241ad36860d5fd56bb77fbf1e9065
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-18 03:44:52 +0100

    init size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
         size: size of the integer
         """
         if type(size) is not int:
-            raise("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
