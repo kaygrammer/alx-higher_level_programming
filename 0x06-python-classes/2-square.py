11b36311215ab25efc343e422e69279cde73ac01
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-18 03:43:09 +0100

    init size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,8 +6,12 @@
 class Square:
     """init size and check for errors"""
     def __init__(self, size=0):
+        """
+        Args:
+        size: size of the integer
+        """
         if type(size) is not int:
             raise("size must be an integer")
-        elif(size < 0):
+        elif size < 0:
             raise("size must be >= 0")
         self.__size = size
