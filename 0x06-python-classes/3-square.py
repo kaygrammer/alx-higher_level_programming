5e09308e55c06b665b45a5d5541662d9c68abd9b
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-18 03:54:23 +0100

    init size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""an empty class that represent a square class"""
+
+
+class Square:
+    """initialize a private instance and public method"""
+    def __init__(self, size=0):
+        """
+        Args:
+        size: size of the integer
+        Area: Area of a square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
