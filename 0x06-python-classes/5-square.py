d2bfb54714e1e1461a7dfe1f61b3cc418e98b08b
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-04-27 16:12:57 +0100

    task 4

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.getter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value   
