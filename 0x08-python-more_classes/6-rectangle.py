b357786534dd655ef3fe6cb562aed65245448b6c
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-05-04 06:38:46 +0100

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,8 +4,8 @@
 
 
 class Rectangle():
-    number_of_instances = 0
     """initiate a Rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """Initialize the rectangle.
         Args:
