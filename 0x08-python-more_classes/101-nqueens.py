7c033259257abd56966726a9411b040decbf9a53
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-05-11 12:57:23 +0100

    task x

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+"""Solves the N-queens puzzle"""
+
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    return [[' ' for _ in range(n)] for _ in range(n)]
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    return [row.copy() for row in board]
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    return [[r, c] for r, row in enumerate(board) for c, val in enumerate(row) if val == "Q"]
+
+
+def mark_attacked_spots(board, row, col):
+    """Mark spots on a chessboard that are under attack by a queen.
+    """
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+
+
+def recursive_solve(board, row, queens, solutions):
+    """Recursively solve an N-queens puzzle.
+    """
+    if queens == len(board):
+        solutions.append(get_solution(board))
+        return solutions
+
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_deepcopy(board)
+            tmp_board[row][c] = "Q"
+            mark_attacked_spots(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1, queens + 1, solutions)
+
+    return solutions
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = init_board(n)
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
